18189d46c7ad7dfe40efae37fd3b29b833e0d8ba
Ecta <gcools@student.42.fr>
2025-05-16 10:56:09 +0200

    un cgi qui fonctionne ?

--- Makefile
+++ Makefile
@@ -20,6 +20,7 @@ SRC             :=      main.cpp \
 						Config/ParserConfig.cpp Config/BlocServer.cpp \
 						Config/BlocLocation.cpp \
 						Logger/Logger.cpp \
+						CGI/cgi.cpp \
 
 
 SRCPATH         :=      ./srcs/
---
+++ srcs/CGI/cgi.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cgi.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcools <gcools@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 11:54:43 by marvin            #+#    #+#             */
+/*   Updated: 2025/05/14 11:54:43 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cgi.hpp"
+
+CGI::CGI(std::string script, std::string query, std::string addr, std::string port,
+         std::string name, std::string srv_port)
+    : script(script), query(query), addr(addr), port(port), server_name(name), server_port(srv_port) {}
+
+void CGI::execute() {
+    std::string interpreter;
+    if (script.substr(script.find_last_of(".") + 1) == "php") {
+        interpreter = "php";
+    } else if (script.substr(script.find_last_of(".") + 1) == "py") {
+        interpreter = "python3";
+    } else {
+        std::cerr << "Type de script non supporté." << std::endl;
+        return;
+    }
+
+    setenv("QUERY_STRING", query.c_str(), 1);
+    setenv("REMOTE_ADDR", addr.c_str(), 1);
+    setenv("REMOTE_PORT", port.c_str(), 1);
+    setenv("SERVER_NAME", server_name.c_str(), 1);
+    setenv("SERVER_PORT", server_port.c_str(), 1);
+
+    char* const argv[] = { const_cast<char*>(interpreter.c_str()), const_cast<char*>(script.c_str()), NULL };
+
+    pid_t pid = fork();
+    if (pid == 0) {
+        if (execvp(interpreter.c_str(), argv) == -1) {
+            std::cerr << "Erreur lors de l'exécution de " << interpreter << " pour le script " << script << std::endl;
+            exit(1);
+        }
+    } else if (pid < 0) {
+        std::cerr << "Erreur lors du fork" << std::endl;
+    } else {
+        waitpid(pid, NULL, 0);
+    }
+}
+
+/* a changer pour faire une fonction qui recup les infos du parsing et qui exec le reste avec 
+int main() {
+    std::string script = "test.py";  // Parsing
+    
+    // Définir la requête CGI
+    std::string query = "name=test&age=42";
+    
+    // Parsing 
+    std::string addr = "127.0.0.1";
+    std::string port = "12345";
+    std::string server_name = "localhost";
+    std::string server_port = "4242";
+    
+    CGI cgi(script, query, addr, port, server_name, server_port);
+    cgi.execute();
+    
+    std::cout << "La requête CGI a été exécutée." << std::endl;
+    
+    return 0;
+}
+*/
